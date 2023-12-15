feccee1b7f20c7d1526ab0e61d79a474ad07f370
androosk <atirpok3@gmail.com>
2023-12-15 12:45:13 -0700

    progress on front end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,93 @@
 import Image from "next/image";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
     <main className="">
+      <div
+        className={`${montserrat.className} container w-10/12 mx-auto mt-28 rounded-t-lg p-4 bg-[#faf9f8]`}
+      >
+        <div className="md:grid md:grid-cols-2 gap-4 items-center justify-center">
+          <div className="px-10">
+            <p className="font-thin mb-7 tracking-wider text-black">
+              DO YOUR LAUNDRY IN TEN MINUTES
+            </p>
+            <p className="text-5xl text-[#1d6076]">Freedom from</p>
+            <p className="text-5xl text-[#1d6076]">Laundry Day.</p>
+            <p className="mt-7 text-xl text-black">Five minutes to drop off,</p>
+            <p className="text-xl text-black">Five minutes to pick up</p>
+          </div>
+          <div>
+            <img
+              className="rounded-full"
+              src={"/images/autri-taheri-xoVylccwsN8-unsplash.jpg"}
+              alt="girl reclining in laundry basket"
+            />
+          </div>
+        </div>
+        <section className="container w-10/12 mx-auto py-12 bg-primary md:flex md:flex-wrap justify-center">
+          <div className="md:w-1/3">
+            <img
+              className="w-3/4 rounded-full mx-auto"
+              src={"/images/scarves-4128126_640.jpg"}
+              alt="images of ties and cufflinks"
+            />
+            <p className="text-center mt-7 text-xl">Fine Dry Cleaning</p>
+            <p className="text-center font-thin py-6 px-8 hidden md:block">
+              Tirpok Cleaners offers the finest dry cleaning services you’ll
+              find. Our attention to detail and complete expertise ensure can
+              trust us with your entire wardrobe.
+            </p>
+          </div>
+          <div className="md:w-1/3">
+            <img
+              className="w-3/4 rounded-full mx-auto"
+              src={"/images/shirts-591750_640.jpg"}
+              alt="images of ties and cufflinks"
+            />
+            <p className="text-center mt-7 text-xl">Platinum Shirt Laundry</p>
+            <p className="text-center font-thin py-6 px-8 hidden md:block">
+              We guarantee that your buttons will be replaced if cracked or
+              broken, whites will be white, colors bright and all removable
+              stains gone
+            </p>
+          </div>
+          <div className="md:w-1/3">
+            <img
+              className="w-3/4 rounded-full mx-auto"
+              src={"/images/jordan-bigelow-53BjYSxca5g-unsplash.jpg"}
+              alt="images of ties and cufflinks"
+            />
+            <p className="text-center mt-7 text-xl">
+              Blankets, Bedspreads and More
+            </p>
+            <p className="text-center font-thin py-6 px-8 hidden md:block">
+              Let us take care of your larger items and downs. They'll come back
+              to you clean, fluffy and fresh every time without the hassle of
+              overloading your washer or spending the day at the laundromat
+            </p>
+          </div>
+          <div className="md:w-1/3">
+            <img
+              className="w-3/4 rounded-full mx-auto"
+              src={"/images/pina-messina-qQKv7r1BaRw-unsplash.jpg"}
+              alt="images of ties and cufflinks"
+            />
+            <p className="text-center mt-7 text-xl">
+              Sew what? We sew anything!
+            </p>
+            <p className="text-center font-thin py-6 px-8 hidden md:block">
+              Check out our expert tailoring service where you can get your
+              pants taken up, replace a zipper or just about any other repair or
+              alteration you may need.
+            </p>
+          </div>
+        </section>
+      </div>
       <video
         autoPlay
         loop
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,37 +2,34 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
   const [navBar, setNavbar] = useState(false);
-  const [hamburger, setHamburger] = useState(false);
 
   const changeBackground = () => {
     if (window.scrollY >= 80) {
+      console.log("navbar active");
       setNavbar(true);
     } else {
       setNavbar(false);
     }
   };
 
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname === path ? "underline underline-offset-4" : "";
+  };
+
   const initial =
     "transition lg:px-16 px-6 flex flex-wrap items-center lg:py-0 py-2 fixed w-full z-10 top-0";
   const active =
-    "transition duration-500 lg:px-16 px-6 flex flex-wrap items-center lg:py-0 py-2 fixed w-full z-10 top-0 bg-primary bg-opacity-80";
+    "transition duration-500 lg:px-16 px-6 flex flex-wrap items-center lg:py-0 py-2 fixed w-full z-10 top-0 bg-white bg-opacity-80";
 
   useEffect(() => {
     window.addEventListener("scroll", changeBackground);
   }, []);
-
-  const handleClick = () => {
-    if (hamburger) {
-      setHamburger(!hamburger);
-      console.log("yo tamborger");
-    } else {
-      setHamburger(!hamburger);
-      console.log("da button was clicked");
-    }
-  };
 
   return (
     <div className={navBar ? active : initial}>
@@ -41,19 +38,19 @@ const NavBar = () => {
           className="h-20 m-2 float-left"
           src="/images/tirpok-logo.png"
           alt="hanger with text tirpok more than just a cleaners"
-          height={300 / 4}
-          width={600 / 4}
+          height={96}
+          width={114}
         />
       </div>
 
-      <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
+      <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
         <svg
           className="fill-current text-gray-900"
           xmlns="http://www.w3.org/2000/svg"
           width="20"
           height="20"
           viewBox="0 0 20 20"
-          onClick={handleClick}
+          // onClick={handleClick}
         >
           <title>menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
